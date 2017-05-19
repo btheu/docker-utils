77379f823c6ab325f7b803ffb3077455a522728f
Benoit Theunissen <benoit.theunissen@gmail.com>
2017-05-19 10:08:29 -0400

    feat: build with volume, and plugin git updated

--- plugins/git.sh
+++ plugins/git.sh
@@ -6,16 +6,25 @@ BUILDER_GIT_URL=
 
 BUILDER_GIT_BRANCH=
 
-BUILDER_GIT_REPO=$BUILDER_TARGET
+BUILDER_GIT_SSH_KEY="$(dirname ~/.ssh/id_rsa.pub)"
+
+l_git(){
+
+  docker run -it --rm -v $BUILDER_VOLUME:/source -w /source -v $BUILDER_GIT_SSH_KEY:/root/.ssh/ indiehosters/git git $@
+
+}
 
 d_git(){
 
-  git clone $BUILDER_GIT_URL $BUILDER_GIT_REPO || echo "Passing"
+  echo "BUILDER_VOLUME        $BUILDER_VOLUME"
+  echo "BUILDER_GIT_URL       $BUILDER_GIT_URL"
+  echo "BUILDER_GIT_BRANCH    $BUILDER_GIT_BRANCH"
+  echo "BUILDER_GIT_SSH_KEY   $BUILDER_GIT_SSH_KEY"
+
+  l_git clone $BUILDER_GIT_URL /source || echo "Passing"
   
-  cd $BUILDER_GIT_REPO
+  l_git fetch --all
   
-  git fetch --all
-  
-  git checkout $BUILDER_GIT_BRANCH ; git pull
+  l_git checkout $BUILDER_GIT_BRANCH ; l_git pull
 }
 
